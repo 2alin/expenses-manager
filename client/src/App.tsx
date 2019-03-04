5eeeb574a826afaaba6d566023dc149c8cd61642
2alin <adilsonsdev@gmail.com>
2019-03-04 00:18:39 -0600

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import ExpensesHistory from './components/ExpensesHistory';
+
+function App() {
+  return (
+    <div>
+      <h1>Expenses Manager</h1>
+      <ExpensesHistory />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ExpensesHistory.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExpensesHistory() {
+  return (
+    <div>
+      <p>Breakfast $15.99</p>
+    </div>
+  )
+}
+
+export default ExpensesHistory
